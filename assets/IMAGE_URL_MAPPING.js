e5ed0a23361b505a859c7289cb1f4b6104768a7b
Muhammad Akhyar Putra Pasya <salahokeh12@gmail.com>
2026-01-02 19:11:35 +0700

    Add files via upload

---
+++ assets/IMAGE_URL_MAPPING.js
@@ -0,0 +1,41 @@
+// Script untuk menambahkan URL gambar ke JSON tanaman
+// Jalankan ini untuk referensi mapping gambar ke URL publik
+
+const imageMapping = {
+  // ASIA
+  "padi-sawah.jpg": "https://images.unsplash.com/photo-1530836369250-ef72a3f5cda8?w=400",
+  "bambu.jpg": "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=400",
+  "lotus-seroja.jpg": "https://images.unsplash.com/photo-1567881997560-d45c8ed4f16a?w=400",
+  "teh-camellia.jpg": "https://images.unsplash.com/photo-1597318371403-d83e5ef07511?w=400",
+  "pohon-bodhi.jpg": "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=400",
+  "sakura.jpg": "https://images.unsplash.com/photo-1522383489734-116ce907aae0?w=400",
+  "peony.jpg": "https://images.unsplash.com/photo-1567881997560-d45c8ed4f16a?w=400",
+  
+  // AFRICA
+  "pohon-baobab.jpg": "https://images.unsplash.com/photo-1505228395891-9a51e7e86e81?w=400",
+  "king-protea.jpg": "https://images.unsplash.com/photo-1618895550302-0e9ee7b62f94?w=400",
+  "bird-of-paradise.jpg": "https://images.unsplash.com/photo-1585865527665-6267baa4a9d0?w=400",
+  "lidah-buaya.jpg": "https://images.unsplash.com/photo-1590080876919-e7b4cc23bb3a?w=400",
+  
+  // AMERICAS
+  "saguaro-cactus.jpg": "https://images.unsplash.com/photo-1582406473519-c3fb148e4662?w=400",
+  "venus-flytrap.jpg": "https://images.unsplash.com/photo-1598999150042-8c7f2a0fb0c0?w=400",
+  "bunga-matahari.jpg": "https://images.unsplash.com/photo-1578927314433-40a4794e4cee?w=400",
+  "alpukat.jpg": "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?w=400",
+  "cabai.jpg": "https://images.unsplash.com/photo-1599599810694-b5ac4dd33c86?w=400",
+  "pohon-redwood.jpg": "https://images.unsplash.com/photo-1511497584788-876760111969?w=400",
+  
+  // AUSTRALIA
+  "eucalyptus-pelangi.jpg": "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=400",
+  "golden-wattle.jpg": "https://images.unsplash.com/photo-1490750967868-88aa4486c946?w=400",
+  "waratah.jpg": "https://images.unsplash.com/photo-1613324808055-ffc9b896a3f1?w=400",
+  "kangaroo-paw.jpg": "https://images.unsplash.com/photo-1540202404-a2f29016b523?w=400",
+  "banksia.jpg": "https://images.unsplash.com/photo-1599599810694-b5ac4dd33c86?w=400",
+  "tea-tree.jpg": "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=400",
+  "grevillea.jpg": "https://images.unsplash.com/photo-1578927314433-40a4794e4cee?w=400",
+  
+  // EUROPE
+  "bunga-lavender.jpg": "https://images.unsplash.com/photo-1611339555312-e607c64352fa?w=400",
+  "pohon-oak.jpg": "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=400",
+  "mawar.jpg": "https://images.unsplash.com/photo-1589981425453-e489bad77652?w=400",
+};
